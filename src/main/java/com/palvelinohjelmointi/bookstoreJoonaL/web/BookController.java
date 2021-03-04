ae7b7598d9ed7efa6b57a9b79ddf1e4fa56366cf
DESKTOP-4ARKIPQ\JoonaLaakso <joona.julius.laakso@gmail.com>
2021-03-04 07:49:04 +0200

    REST service

--- src/main/java/com/palvelinohjelmointi/bookstoreJoonaL/web/BookController.java
+++ src/main/java/com/palvelinohjelmointi/bookstoreJoonaL/web/BookController.java
@@ -10,8 +10,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.palvelinohjelmointi.bookstoreJoonaL.domain.*;
+
 
 @Controller
 public class BookController {
@@ -27,6 +29,18 @@ public class BookController {
 		model.addAttribute("books", repository.findAll());
 		return "booklist";
 	}
+	
+	// RESTful service to get all books
+    @RequestMapping(value="/books", method = RequestMethod.GET)
+    public @ResponseBody List<Book> studentListRest() {	
+        return (List<Book>) repository.findAll();
+    }   
+    
+    // RESTful service to get book by id
+    @RequestMapping(value="/book/{id}", method = RequestMethod.GET)
+    public @ResponseBody Optional<Book> findStudentRest(@PathVariable("id") Long bookId) {	
+    	return repository.findById(bookId);
+    } 
 	
 	
 	//add new book
